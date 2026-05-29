2884331adff28ad2885a9370cfd77355a1c8043f
Dhruv Sheth <dhruvsheth@pantheon.inc>
2026-05-29 01:18:10 -0700

    fix: use correct wandb entity name

--- scripts/train_vlm.py
+++ scripts/train_vlm.py
@@ -95,7 +95,7 @@ def main() -> None:
 
     if args.wandb:
         import wandb
-        wandb.init(entity="dsheth_caltech", project="cs148b-hw3", name=run_name, config={
+        wandb.init(entity="dhruvsheth", project="cs148b-hw3", name=run_name, config={
             "injection": args.injection, "mask_mode": args.mask_mode,
             "freeze_config": args.freeze_config, **cfg,
         })
